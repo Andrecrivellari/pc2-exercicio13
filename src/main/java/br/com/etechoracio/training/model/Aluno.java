bef18500b8ac2f19746d73f4d11df26eb1cc5e2c
Andrecrivellari <alcpereira89@gmail.com>
2019-03-22 22:31:27 -0300

    Ex.13,Ex.14,Ex.15

---
+++ src/main/java/br/com/etechoracio/training/model/Aluno.java
@@ -0,0 +1,49 @@
+package br.com.etechoracio.training.model;
+
+import java.util.Date;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import br.com.etechoracio.common.model.BaseORM;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity(name = "EXERCICIO13")
+@Table(name = "TBL_ALUNO")
+public class Aluno extends BaseORM {
+	
+	@Id
+	@GeneratedValue
+	@Column(name = "ID_ALUNO")
+	private Long id;
+
+	@Column(name = "NM_ALUNO")
+	private String nome;
+
+	@Column(name = "NR_CPF")
+	private Long cpf;
+
+	@Column(name = "TX_FONE")
+	private String telefone;
+
+	@Column(name = "TX_EMAIL")
+	private String email;
+
+	@Column(name = "DT_CRIACAO")
+	@DateTimeFormat
+	private Date dataCriacao;
+	
+	@OneToOne(mappedBy="aluno", cascade = CascadeType.ALL)
+	private Endereco endereco = new Endereco();
+
+}
